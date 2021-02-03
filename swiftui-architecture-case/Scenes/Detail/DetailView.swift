3ec2a41da8a44e705bffe5663e4ba774bf336f31
Bertuğ <bertug.yilmaz@d-teknoloji.com.tr>
2021-02-03 16:52:16 +0300

    Initialize project

---
+++ swiftui-architecture-case/Scenes/Detail/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  swiftui-architecture-case
+//
+//  Created by Bertug Yilmaz (Dogus Teknoloji) on 2/3/21.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
