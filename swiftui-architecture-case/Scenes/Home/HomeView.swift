a2b28c4abb55fedaecfaeed3f59b8a7abe781fbf
Bertuğ <bertug.yilmaz@d-teknoloji.com.tr>
2021-02-03 17:02:02 +0300

    Added some router sample with NavigationLink

--- swiftui-architecture-case/Scenes/Home/HomeView.swift
+++ swiftui-architecture-case/Scenes/Home/HomeView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView(content: {
+            HomeRouter(route: .detail(content: "I am comin")) {
+                Text("Navigate me")
+            }
+        })
     }
 }
 
